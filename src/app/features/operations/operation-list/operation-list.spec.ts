2485be582a76827255b7634344b9d7b695da7f5b
Bota93 <a.alcaraz.rodri@gmail.com>
2026-05-03 16:56:40 +0200

    feat: connect operation list to service with real data

--- src/app/features/operations/operation-list/operation-list.spec.ts
+++ src/app/features/operations/operation-list/operation-list.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { OperationList } from './operation-list';
 
@@ -9,6 +10,7 @@ describe('OperationList', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [OperationList],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(OperationList);
--- src/app/features/operations/operation-list/operation-list.ts
+++ src/app/features/operations/operation-list/operation-list.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { OperationService } from '../../../core/services/operation';
 
 @Component({
   selector: 'app-operation-list',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './operation-list.html',
   styleUrl: './operation-list.scss',
 })
-export class OperationList {}
+export class OperationList {
+  constructor(public operationService: OperationService) {}
+
+  deleteOperation(id: string): void {
+    const confirmed = confirm('Are you sure you want to delete this operation?');
+
+    if (!confirmed) {
+      return;
+    }
+
+    this.operationService.deleteOperation(id);
+  }
+}
